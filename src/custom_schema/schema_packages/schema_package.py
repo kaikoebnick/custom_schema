133ef95c89610eac16544154f783bb30a6301cc7
kaikoebnick <kai.koebnick@gmail.com>
2024-10-07 16:12:57 +0200

    Update schema_package.py

--- src/custom_schema/schema_packages/schema_package.py
+++ src/custom_schema/schema_packages/schema_package.py
@@ -27,27 +27,50 @@ m_package = SchemaPackage()
 
 
 class NewSchemaPackage(Schema):
-    name = Quantity(
-        type=str, a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity)
+    meausrement_name = Quantity(
+        type=str, 
+        description='''A nice and short name for your measurement.''',
+        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity)
     )
-    author = Quantity(
-        type=Author, a_eln=ELNAnnotation(component=ELNComponentEnum.AuthorEditQuantity)
-    ) # TODO: default current user
-    date_time = Quantity(
+    datetime = Quantity(
         type=Datetime,
+        description='''Date and time the measurement was taken.''',
+        #default=
         a_eln=ELNAnnotation(component=ELNComponentEnum.DateTimeEditQuantity)
     )
-    rich_text = Quantity(
+    nice_peak = Quantity(
+        type=bool,
+        description='''Check if there is a nice peak in the measurement.''',
+        a_eln=ELNAnnotation(component=ELNComponentEnum.BoolEditQuantity) #default
+    )
+    measurement_category = Quantity(
+        type=(
+            type_kind=enum,
+            type_data={
+              "brand", "Ford",
+            }
+        ),
+        description='''Choose your category.''',
+        #default=
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.RadioEnumEditQuantity,
+        )
+    )
+    description = Quantity(
         type=str,
+        description='''You can add text as well as pictures.''',
         a_eln=ELNAnnotation(component=ELNComponentEnum.RichTextEditQuantity)
     )
-    sample_id = Quantity(type=str)
+    sample_id = Quantity(
+        type=str,
+        description='''The ID will be automatically generated.''',
+    )
 
     def normalize(self, archive, logger):
         super(Schema, self).normalize(archive, logger)
 
         if self.sample_id is None:
-            self.sample_id = f'{self.added_date}--{self.formula}'
+            self.sample_id = f'{self.datetime}--{self.name}'
 
 
 m_package.__init_metainfo__()
